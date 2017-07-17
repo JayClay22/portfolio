2d375308b6917f1fa6aa6ff9120f9b4c6cde73e5
Jordan Clay <jordan.clay@blackburn.edu>
2017-07-17 13:54:48 -0500

    Made another change to my sign in page

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
-function alertMe(){
+function alertMe() {
     alert('Your form has been Submitted!')
 }
 function submitContactForm() {
     alert('Thank you for submitting the form')
+}
+$(document).ready(function () {
+
+    $('#contactForm').on('submit', submitContactform);
+
+});
+
+function submitContactform() {
+    event.preventDefault();
+    var x = document.forms["contactForm"]["email"].value;
+    if (x == "") {
+        alert("Email must be filled out");
+        return false;
+    } else {
+        $(this).fadeOut();
+        $("#success").fadeIn();
+        //alert('Thank you for submitting the form');
+    }
 }
