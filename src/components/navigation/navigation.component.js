b4e65b7f886ac89f261afbe319656b857aebe7eb
Jordan Clay <jordan.clay@blackburn.edu>
2017-07-24 13:30:10 -0500

    Chnages made to navigation

---
+++ src/components/navigation/navigation.component.js
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navigation',
+  templateURL: 'dist/html/navigation.html'
+})
+
+export class NavigationComponent {
+      name = 'Jordan Clay';
+}
